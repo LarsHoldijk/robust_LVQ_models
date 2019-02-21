c590ef794f6dc8c5273f8e2f204635ad53b793e2
LarsHoldijk <larsholdijk@gmail.com>
2019-02-21 22:34:18 +0100

    Optimized parameter name and code usage

--- foolbox_model.py
+++ foolbox_model.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
 import foolbox
-from robust_LVQ_models.models import cnn, glvq, glvq_large, gmlvq, gmlvq_large, gtlvq, gtlvq_large
+from robust_LVQ_models.tensorflow_graph import get_tensorflow_graph
 
 
-def create(**kwargs):
+def create(model_name, large_model=False):
     '''
     Instantiates a foolbox model for the required model
     :param model_name: name of the model that should be initiated
+    :param large_model: whether or not the model with more prototypes per class should be used
     :return: foolbox model
     '''
-    if 'model_name' not in kwargs:
-        raise Exception('Model_name parameter is required')
-    model_name = kwargs['model_name']
-    if model_name == 'cnn':
-        graph, input, output = cnn()
-    elif model_name == 'glvq':
-        graph, input, output = glvq()
-    elif model_name == 'glvq_large':
-        graph, input, output = glvq_large()
-    elif model_name == 'gmlvq':
-        graph, input, output = gmlvq()
-    elif model_name == 'gmlvq_large':
-        graph, input, output = gmlvq_large()
-    elif model_name == 'gtlvq':
-        graph, input, output = gtlvq()
-    elif model_name == 'gtlvq_large':
-        graph, input, output = gtlvq_large()
-    else:
-        raise Exception('Model_name unknown')
+
+    graph, input, output = get_tensorflow_graph(model_name, large_model)
     fmodel = foolbox.models.TensorFlowModel(input, output, bounds=(0, 255), preprocessing=(0, 255))
     return fmodel
--- tensorflow_graph.py
+++ tensorflow_graph.py
@@ -3,25 +3,26 @@
 from robust_LVQ_models.models import cnn, glvq, glvq_large, gmlvq, gmlvq_large, gtlvq, gtlvq_large
 
 
-def get_tensorflow_graph(model_name):
+def get_tensorflow_graph(model_name, large_model=False):
     '''
     Instantiates a tensorflow graph for the required model
     :param model_name: name of the model that should be initiated
+    :param large_model: whether or not the model with more prototypes per class should be used
     :return: foolbox graph, input tensor and output tensor of the graph
     '''
     if model_name == 'cnn':
         graph, input, output = cnn()
     elif model_name == 'glvq':
         graph, input, output = glvq()
-    elif model_name == 'glvq_large':
+    elif model_name == 'glvq' and large_model:
         graph, input, output = glvq_large()
     elif model_name == 'gmlvq':
         graph, input, output = gmlvq()
-    elif model_name == 'gmlvq_large':
+    elif model_name == 'gmlvq' and large_model:
         graph, input, output = gmlvq_large()
     elif model_name == 'gtlvq':
         graph, input, output = gtlvq()
-    elif model_name == 'gtlvq_large':
+    elif model_name == 'gtlvq' and large_model:
         graph, input, output = gtlvq_large()
     else:
         raise Exception('Model_name unknown')
