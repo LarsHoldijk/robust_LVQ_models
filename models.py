26c2edbff931e2aaaa0dd554e679d6d094605a31
LarsHoldijk <larsholdijk@gmail.com>
2019-02-21 22:04:03 +0100

    changed to a different format

--- models.py
+++ models.py
@@ -2,7 +2,13 @@ import os
 
 import tensorflow as tf
 
+
 def load_graph(frozen_graph_filename):
+    '''
+    Loads the tensorflow graph from a .pb file.
+    :param frozen_graph_filename: Path to the .pb file
+    :return: Tensorflow graph
+    '''
     with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
@@ -12,7 +18,12 @@ def load_graph(frozen_graph_filename):
 
     return graph
 
+
 def cnn():
+    '''
+    The baseline CNN with two convolutional and two fully connected layers.
+    :return: Tensorflow graph, input tensor of graph and the graphs output tensor
+    '''
     filename = os.path.join(os.path.dirname(__file__), 'model_files/cnn.pb')
     graph = load_graph(filename)
 
@@ -21,20 +32,86 @@ def cnn():
 
     return graph, input, output
 
+
 def glvq():
-    pass
+    '''
+    The GLVQ model with one prototype per class.
+    :return: Tensorflow graph, input tensor of graph and the graphs output tensor
+    '''
+    filename = os.path.join(os.path.dirname(__file__), 'model_files/glvq.pb')
+    graph = load_graph(filename)
+
+    input = graph.get_tensor_by_name('prefix/encoder_input:0')
+    output = graph.get_tensor_by_name('prefix/lambda_1/Neg:0')
+
+    return graph, input, output
+
 
 def glvq_large():
-    pass
+    '''
+    The GLVQ model with 128 prototypes per class.
+    :return: Tensorflow graph, input tensor of graph and the graphs output tensor
+    '''
+    filename = os.path.join(os.path.dirname(__file__), 'model_files/glvq_large.pb')
+    graph = load_graph(filename)
+
+    input = graph.get_tensor_by_name('prefix/encoder_input:0')
+    output = graph.get_tensor_by_name('prefix/lambda_1/Neg:0')
+
+    return graph, input, output
+
 
 def gmlvq():
-    pass
+    '''
+    The GMLVQ model with one prototype per class.
+    :return: Tensorflow graph, input tensor of graph and the graphs output tensor
+    '''
+    filename = os.path.join(os.path.dirname(__file__), 'model_files/gmlvq.pb')
+    graph = load_graph(filename)
+
+    input = graph.get_tensor_by_name('prefix/encoder_input:0')
+    output = graph.get_tensor_by_name('prefix/lambda_1/Neg:0')
+
+    return graph, input, output
+
 
 def gmlvq_large():
-    pass
+    '''
+    The GMLVQ model with 49 prototypes per class.
+    :return: Tensorflow graph, input tensor of graph and the graphs output tensor
+    '''
+    filename = os.path.join(os.path.dirname(__file__), 'model_files/gmlvq_large.pb')
+    graph = load_graph(filename)
+
+    input = graph.get_tensor_by_name('prefix/encoder_input:0')
+    output = graph.get_tensor_by_name('prefix/lambda_1/Neg:0')
+
+    return graph, input, output
+
 
 def gtlvq():
-    pass
+    '''
+    The GTLVQ model with one prototype for each class.
+    :return: Tensorflow graph, input tensor of graph and the graphs output tensor
+    '''
+    filename = os.path.join(os.path.dirname(__file__), 'model_files/gtlvq.pb')
+    graph = load_graph(filename)
 
-def gtvlq_large():
-    pass
+    input = graph.get_tensor_by_name('prefix/encoder_input:0')
+    output = graph.get_tensor_by_name('prefix/lambda_1/Neg:0')
+
+    return graph, input, output
+
+
+def gtlvq_large():
+    '''
+    The GTLVQ model with 10 prototypes per class.
+    :return: Tensorflow graph, input tensor of graph and the graphs output tensor
+    '''
+    filename = os.path.join(os.path.dirname(__file__), 'model_files/gtlvq_large.pb')
+    graph = load_graph(filename)
+
+    input = graph.get_tensor_by_name('prefix/encoder_input:0')
+    output = graph.get_tensor_by_name('prefix/lambda_1/Neg:0')
+
+    return graph, input, output
--- tensorflow_graph.py
+++ tensorflow_graph.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
-from foolbox import foolbox
 from robust_LVQ_models.models import cnn, glvq, glvq_large, gmlvq, gmlvq_large, gtlvq, gtlvq_large
 
 
-def create(**kwargs):
+def get_tensorflow_graph(model_name):
     '''
-    Instantiates a foolbox model for the required model
+    Instantiates a tensorflow graph for the required model
     :param model_name: name of the model that should be initiated
-    :return: foolbox model
+    :return: foolbox graph, input tensor and output tensor of the graph
     '''
-    if 'model_name' not in kwargs:
-        raise Exception('Model_name parameter is required')
-    model_name = kwargs['model_name']
     if model_name == 'cnn':
         graph, input, output = cnn()
     elif model_name == 'glvq':
@@ -29,5 +25,4 @@ def create(**kwargs):
         graph, input, output = gtlvq_large()
     else:
         raise Exception('Model_name unknown')
-    fmodel = foolbox.models.TensorFlowModel(input, output, bounds=(0, 255), preprocessing=(0, 255))
-    return fmodel
+    return graph, input, output
